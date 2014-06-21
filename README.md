29843c9e7a29e691314425fb2f90bc62de5a9ac7
ryanj <ryan.jarvinen@gmail.com>
2014-06-21 13:01:50 -0700

    creating a basic test case for using add-on carts with php

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
-# A basic Silex server example
-*powered by PHP and Silex*
-
 To host this project locally ([PHP-5.4 or better required](http://us3.php.net/manual/en/features.commandline.webserver.php)), run:
 
     php -S localhost:8080 -t static app.php
 
+You can also pass temporary environment values to a local webserver:
+
+    LEGACY_DB_PASSWORD=P455W0RD php -S localhost:8080 -t static app.php
+
 To spin up a new copy of this application on an OpenShift-powered cloud, use the [`rhc` command line tool](http://rubygems.org/gems/rhc):
 
-    rhc app create silex php-5.4 --from-code=https://github.com/ryanj/silex-base.git
-    
-Or [link to a web-based clone+deploy](https://openshift.redhat.com/app/console/application_type/custom?cartridges%5B%5D=php-5.4&initial_git_url=https%3A%2F%2Fgithub.com%2Fryanj%2Fsilex-base.git) on [OpenShift Online](http://OpenShift.com) or on [your own OpenShift cloud](http://openshift.github.io): 
+    rhc app create dbtest php-5.4 --from-code=https://github.com/ryanj/php-addon-cart-test.git
 
-    https://openshift.redhat.com/app/console/application_type/custom?cartridges%5B%5D=php-5.4&initial_git_url=https%3A%2F%2Fgithub.com%2Fryanj%2Fsilex-base.git
+Visit the resulting application url to check it's initial state.  The DB connection strings should all read: `UNAVAILABLE`
+
+Load an add-on cartridge:
+
+    rhc cartridge add mysql -a dbtest
+
+Then, restart your webserver in order to read the new cart-provided DB connection strings:
+
+    rhc app restart dbtest
+
+Reload your web browser to see the result.
 
 ## License
 This code is dedicated to the public domain to the maximum extent permitted by applicable law, pursuant to CC0 (http://creativecommons.org/publicdomain/zero/1.0/)
--- app.php
+++ app.php
@@ -1,37 +1,15 @@
 <?php
-//Allow PHP's built-in server to serve our static content in local dev:
-if (php_sapi_name() === 'cli-server' && is_file(__DIR__.'/static'.preg_replace('#(\?.*)$#','', $_SERVER['REQUEST_URI']))
-   ) {
-  return false;
-}
-
 require 'vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Response;
 $app = new \Silex\Application();
 
 $app->get('/', function () use ($app) {
-  return $app->sendFile('./static/index.html');
+  $mysql_db_info = getenv('OPENSHIFT_MYSQL_DB_PASSWORD') ? getenv('OPENSHIFT_MYSQL_DB_PASSWORD') : "UNAVAILABLE";
+  $pg_db_info = getenv('OPENSHIFT_POSTGRESQL_DB_PASSWORD') ? getenv('OPENSHIFT_POSTGRESQL_DB_PASSWORD') : "UNAVAILABLE";
+  $mongodb_info = getenv('OPENSHIFT_MONGODB_DB_PASSWORD') ? getenv('OPENSHIFT_MONGODB_DB_PASSWORD') : "UNAVAILABLE";
+  $legacy_db_info = getenv('LEGACY_DB_PASSWORD') ? getenv('LEGACY_DB_PASSWORD') : "UNAVAILABLE";
+  return new Response( "<p>MySQL DB password: {$mysql_db_info}\n<p>Postgres DB password: {$pg_db_info}\n<p>MongoDB password: {$mongodb_info}\n<p>Legacy DB password: {$legacy_db_info}");
 });
-
-$app->get('/hello/{name}', function ($name) use ($app) {
-  //return $app->json( array('Hello'=> $app->escape($name)));
-  return new Response( "Hello, {$app->escape($name)}!");
-});
-
-// the .htaccess file should handle our static content in Production.
-// PHP-5.4's local server can handle static content as well (see README) 
-// This alternate method for serving static files should not be needed: 
-//$app->get('/{folder}/{filename}', function ($folder, $filename) use ($app){
-//  $filepath = __DIR__.'/static/'.$folder.'/'.$filename;
-//  if (!file_exists($filepath)) {
-//    $app->abort(404);
-//  }
-//  if($folder == 'css'){
-//    return $app->sendFile($filepath, 200, array('Content-Type' => 'text/css'));
-//  }else{
-//    return $app->sendFile($filepath, 200);
-//  }
-//});
 
 $app->run();
 ?>
